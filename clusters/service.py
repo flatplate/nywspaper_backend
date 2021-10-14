25267f19e79ae961627f0a4840687e5274a02494
Ural Bayhan <uralbayhan0@gmail.com>
2021-10-14 22:53:16 +0200

    Omit stories having fewer than 3 articles

--- clusters/service.py
+++ clusters/service.py
@@ -1,8 +1,9 @@
-from sqlalchemy import desc, select
+from sqlalchemy import desc, select, func
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session
 
-from .model import Cluster
+from .model import Cluster, Article
+
 
 class StoryService:
     def __init__(self, engine: Engine):
@@ -10,4 +11,10 @@ class StoryService:
 
     def get_top_stories(self, offset=0):
         with Session(self.engine) as session:
-            return [row[0].to_dict() for row in session.execute(select(Cluster).order_by(desc(Cluster.published)).limit(20).offset(offset))]
+            return [row[0].to_dict() for row in session.execute(select(Cluster)
+                                                                .join(Article)
+                                                                .having(func.count(Article.id) > 2)
+                                                                .group_by(Cluster)
+                                                                .order_by(desc(Cluster.published))
+                                                                .limit(20)
+                                                                .offset(offset))]
